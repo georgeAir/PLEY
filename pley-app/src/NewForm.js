f8b28fb24d906253a3284ad2610cb1180017de18
george-arias <georgearias08@gmail.com>
2022-01-26 12:23:40 -0600

    new-form

---
+++ pley-app/src/NewForm.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react'
+
+export default class NewForm extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      name: ''
+    }
+  }
+
+  handleSumbit = (event) => {
+    event.preventDefault()
+    //fetch to the backend
+    fetch(this.props.baseUrl + '/holidays', {
+      method: 'Post',
+      body: JSON.stringify({name: this.state.name}),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    }).then(res => {
+      return res.json()
+    }).then( data => {
+      // console.log(data);
+      this.props.addHoliday(data)
+      this.setState({
+        name: ''
+      })
+    })
+  }
+
+  handleChange = (event) => {
+    // console.log(event.target.value);
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+
+  render(){
+    console.log(this.state.name);
+    return(
+      <form onSubmit= {this.handleSumbit}>
+        <label html='name'>Name: </label>
+        <input type='text' id='name' name='name' onChange={(event) => this.handleChange(event)} value={this.state.name}/>
+        <input type='submit' value='Add a reason to celebrate'/>
+      </form>
+    )
+  }
+}
