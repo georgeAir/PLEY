4d8220d4a4b9601ab671a9ce8e3695a081862621
george-arias <georgearias08@gmail.com>
2022-01-26 16:41:21 -0600

    restaurant show page

---
+++ pley-app/src/restaurantsInfo.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+export default class RestaurantInfo extends Component{
+  render(){
+    return(
+      <>
+        <h1>name: {this.props.city.name}</h1>
+        <h2>cuisineType: {this.props.cuisineType}</h2>
+        <h2>description: {this.props.description}</h2>
+        <h2>likes: {this.props.likes}</h2>
+        <h2>location: {this.props.location}</h2>
+
+      </>
+    )
+  }
+}
