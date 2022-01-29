44697b826f97d540a786c24cc79b6382a16626da
marcwolfe <marc.wolfe21@gmail.com>
2022-01-29 13:41:14 -0600

    newform updated

--- src/NewForm.js
+++ src/NewForm.js
@@ -1,50 +1,76 @@
 import React, {Component} from 'react'
 
-
 export default class NewForm extends Component {
-    constructor(props){
-        super(props)
+  constructor(){
+    super()
 
-        this.state = {
-            name:''
-        }
+    this.state = {
+      name: '',
+      price: '',
+      likes: '',
+      img: '',
+      phone: '',
+      display_address: '',
+      timestamps:'',
     }
+  }
 
-    handleSubmit = (event) => {
-        event.preventDefault()
-        //fetch to the backend
-        fetch(this.props.baseURL + '/favorites' , {
-            method: 'POST',
-            body: JSON.stringify({name: this.state.name}),
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            credentials: 'include'
-        }).then(res => {
-            return res.json()
-        }).then(data => {
-            // console.log(data)
-            this.props.addFavorite(data)
-            this.setState({
-                name: ''
-            })
-        })
-    }
 
-    handleChange = (event) => {
-        // console.log(event.target.value);
-        this.setState({
-            name: event.target.value
-        })
-    }
+  handleSumbit = (event) => {
+    event.preventDefault()
+    //fetch to the backend
+    fetch(this.props.baseUrl + '/favorites', {
+      method: 'Post',
+      body: JSON.stringify({name: this.state.name}),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    }).then(res => {
+      return res.json()
+    }).then( data => {
+      // console.log(data);
+      this.props.addRestaurant(data)
+      this.setState({
+        name: ''
+      })
+    })
+  }
 
-    render() {
-        return(
-            <form onSubmit={this.handleSubmit}>
-                <label htmlFor="name">Name: </label>
-                <input type="text" id="name" name="name" onChange={ (event) => this.handleChange(event)} value={this.state.name}/>
-                <input type="submit" value="Add a reason to celebrate"/>
-            </form>
-        )
-    }
+  handleChange = (event) => {
+    console.log(event.target.value);
+    this.setState({
+      [event.target.id]: event.target.value,
+    })
+  }
+
+
+  render(){
+    console.log(this.state);
+    return(
+      <>
+        <form onSubmit= {this.handleSumbit}>
+          <label html='name'>Name: </label>
+          <input type='text' id='name' name='name' onChange={(event) => this.handleChange(event)} value={this.state.name}/>
+
+          <label html='name'>Cuisine Type: </label>
+          <input type='text' id='cuisineType' name='cuisineType' onChange={(event) => this.handleChange(event)} value={this.state.price}/>
+
+          <label html='name'>Location: </label>
+          <input type='text' id='location' name='name' onChange={(event) => this.handleChange(event)} value={this.state.likes}/>
+
+          <label html='name'>Description: </label>
+          <input type='text' id='description' name='name' onChange={(event) => this.handleChange(event)} value={this.state.img}/>
+
+          <label html='name'>Likes: </label>
+          <input type='text' id='likes' name='name' onChange={(event) => this.handleChange(event)} value={this.state.phone}/>
+
+          <label html='name'>Likes: </label>
+          <input type='text' id='likes' name='name' onChange={(event) => this.handleChange(event)} value={this.state.display_address}/>
+
+          <input type='submit' value='Add a restaurant'/>
+        </form>
+      </>
+
+    )
+  }
 }
